d4c39fed7b1ce335e2b150fc4aff538bed7f5de0
cswongaq <cswongaq@ust.hk>
2016-11-09 12:00:33 +0800

    Update index.js
    make the index.js function can be called

--- app/js/index.js
+++ app/js/index.js
@@ -1,32 +1,39 @@
-$(document).ready(function(){
+var click = [
+  function(){
+    var val = $('#input_text').val();
+    if ( val !== '' ) {
+      var url = "admin.html?q=" + val;
+      window.location.href= url ;
+      return false;
+    }
+  },
+  function(){
+    var val = $('#input_text').val();
+    if ( val !== '' ) {
+      var url = "team.html?q=" + val;
+      window.location.href= url ;
+      return false;
+    }
+  },
+  function(){
+    var val = $('#input_text').val();
+    if ( val !== '' ) {
+      var url = "member.html?q=" + val;
+      window.location.href= url ;
+      return false;
+    }
+  }
+];
 
 
-    $("#btn_admin").click(function(){
-    	var val = $('#input_text').val();
-    	if ( val !== '' ) {
-    		var url = "admin.html?q=" + val;
-    		window.location.href= url ;
-    		return false;
-    	}
-    });
+var onReady = function(){
 
-    $("#btn_leader").click(function(){
-    	var val = $('#input_text').val();
-    	if ( val !== '' ) {
-    		var url = "team.html?q=" + val;
-    		window.location.href= url ;
-    		return false;
-    	}
-    });
+  $("#btn_admin").click(click[0]);
 
-    $("#btn_member").click(function(){
-    	var val = $('#input_text').val();
-    	if ( val !== '' ) {
-    		var url = "member.html?q=" + val;
-    		window.location.href= url ;
-    		return false;
-    	}
-    });
+  $("#btn_leader").click(click[1]);
 
+  $("#btn_member").click(click[2]);
 
-});
+}
+
+$(document).ready(onReady);
